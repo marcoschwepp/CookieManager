98fa1b9baa37950eff0048de53783e57622e42cd
Marco Schweppe <Schweppe.M.EIT92@rvwbk.de>
2022-10-09 23:00:35 +0200

    added tests

--- tests/CookieTest.php
+++ tests/CookieTest.php
@@ -12,7 +12,29 @@ final class CookieTest extends TestCase
 
         self::assertSame($newCookie->getName(), 'testCookie');
         self::assertSame($newCookie->getExpires(), 0);
+        self::assertSame($newCookie->getPath(), '/');
+        
+        self::assertFalse($newCookie->isSecure());
+        self::assertFalse($newCookie->isHttpOnly());
 
         self::assertEmpty($newCookie->getValue());
+    }
+
+    public function testConstructFromOptions(): void
+    {
+        $options = [
+            'name' => 'testCookie',
+        ];
+
+        $newCookieFromOptions = marcoschwepp\Cookie\Cookie::constructFromOptions($options);
+
+        self::assertSame($newCookieFromOptions->getName(), 'testCookie');
+        self::assertSame($newCookieFromOptions->getExpires(), 0);
+        self::assertSame($newCookieFromOptions->getPath(), '/');
+        
+        self::assertFalse($newCookieFromOptions->isSecure());
+        self::assertFalse($newCookieFromOptions->isHttpOnly());
+
+        self::assertEmpty($newCookieFromOptions->getValue());
     }
 }
