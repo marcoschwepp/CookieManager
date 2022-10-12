ed188a96aa547f022a524186aa0ec93898ff31de
Hendrik Legge <admin@themepoint.de>
2022-10-12 18:47:27 +0200

    Decreased complexity of UtilityTest

--- tests/CookieUtilityTest.php
+++ tests/CookieUtilityTest.php
@@ -19,31 +19,31 @@ use PHPUnit\Framework\TestCase;
  */
 final class CookieUtilityTest extends TestCase
 {
-    public function testNormalizeDomain(): void
+	/** @dataProvider domainProvider */
+    public function testNormalizeDomain(string $input, ?string $expected): void
     {
-        $domain1 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('www.google.de');
-        $domain2 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('www.google');
-        $domain3 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('google.de');
-        $domain4 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('domain-@test.@de');
-        $domain5 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('xn--fsqu00a.xn--0zwm56d');
-        $domain6 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('..');
-        $domain7 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('.');
-        $domain8 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('-_-');
-        $domain9 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('---');
-        $domain10 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('');
-        $domain11 = marcoschwepp\Cookie\CookieUtility::normalizeDomain('www.test.de.');
+		$normalizedDomain = marcoschwepp\Cookie\CookieUtility::normalizeDomain($input);
 
-        self::assertSame($domain1, '.www.google.de');
-        self::assertSame($domain2, '.www.google');
-        self::assertSame($domain3, '.google.de');
-        self::assertSame($domain5, '.xn--fsqu00a.xn--0zwm56d');
-
-        self::assertNull($domain4);
-        self::assertNull($domain6);
-        self::assertNull($domain7);
-        self::assertNull($domain8);
-        self::assertNull($domain9);
-        self::assertNull($domain10);
-        self::assertNull($domain11);
+		self::assertEquals($expected, $normalizedDomain);
     }
+
+	public function domainProvider(): array
+	{
+		return [
+			['www.google.de', '.www.google.de'],
+			['www.google', '.www.google'],
+			['google.de', '.google.de'],
+			['domain-@test.@de', null],
+			['xn--fsqu00a.xn--0zwm56d', '.xn--fsqu00a.xn--0zwm56d'],
+			['..', null],
+			['.', null],
+			['-_-', null],
+			['---', null],
+			['', null],
+			['www.test.de.', null],
+			['ab', null],
+			['https://google.com', '.google.com'],
+			['https://www.google.com', '.www.google.com'],
+		];
+	}
 }
