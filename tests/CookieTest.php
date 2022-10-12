05a2a16bacb0c74973da798c0052976a4b34bdb3
Hendrik Legge <admin@themepoint.de>
2022-10-12 22:55:56 +0200

    Fixed testCookieIsExpired

--- tests/CookieTest.php
+++ tests/CookieTest.php
@@ -150,8 +150,9 @@ final class CookieTest extends TestCase
 			'name' => $name,
 		];
 
-		$expires = new DateTimeImmutable('now');
-		$expires = $expires->modify('+1 day');
+		$faker = Faker\Factory::create();
+
+		$expires = new DateTimeImmutable($faker->date('Y-m-d H:i:s', '+1 year'));
 
 		$cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
 
@@ -168,29 +169,43 @@ final class CookieTest extends TestCase
 		$options = [
 			'name' => $name,
 		];
+		$faker = Faker\Factory::create();
 		$cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
-		$cookie->expiresIn(86400); // use data provider for that?
-		$formatedDate = new \DateTimeImmutable(sprintf('+ %s seconds', 86400));
-		$formatedDate = $formatedDate->format('Y-m-d H:i:s');
+		$seconds = $faker->numberBetween(1000, 999999999);
+		$cookie->expiresIn($seconds);
+		$expectedDate = new \DateTimeImmutable(sprintf('+ %s seconds', $seconds));
 
-		self::assertSame($formatedDate, $cookie->getExpiresAt()->format('Y-m-d H:i:s'));
+		self::assertSame($expectedDate->format('Y-m-d H:i:s'), $cookie->getExpiresAt()->format('Y-m-d H:i:s'));
 	}
 
 	/**
-	 * @dataProvider \Ergebnis\DataProvider\StringProvider::arbitrary()
+	 * @dataProvider expiredDataProvider
 	 */
-	public function testCookieIsExpired(string $name): void
+	public function testCookieIsExpired(string $modifier, bool $expected): void
 	{
+		$faker = Faker\Factory::create();
 		$options = [
-			'name' => $name,
+			'name' => $faker->word(),
 		];
-		$expiredCookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
-		$validCookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
+		$cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
 
-		$expiredCookie->expiresAt(new \DateTimeImmutable(sprintf('- %s day', 1)));
-		$validCookie->expiresAt(new \DateTimeImmutable(sprintf('+ %s day', 1)));
+		$cookie->expiresAt(new \DateTimeImmutable($modifier));
 
-		self::assertTrue($expiredCookie->isExpired());
-		self::assertFalse($validCookie->isExpired());
+		self::assertSame($expected, $cookie->isExpired());
+	}
+
+	public function expiredDataProvider(): array
+	{
+		$faker = Faker\Factory::create();
+		$days = $faker->numberBetween(2, 999);
+		$minutes = $faker->numberBetween(1, 59);
+		return [
+			[\sprintf('- %s days', $days), true],
+			[\sprintf('+ %s days', $days), false],
+			['- 1 day', true],
+			['+ 1 day', false],
+			[\sprintf('- %s minutes', $minutes), true],
+			[\sprintf('+ %s minutes', $minutes), false],
+		];
 	}
 }
