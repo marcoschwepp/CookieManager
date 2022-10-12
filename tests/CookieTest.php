487af32a50ac2239b445b6a46ad5bf8b311e4f86
Hendrik Legge <admin@themepoint.de>
2022-10-12 23:12:12 +0200

    Rebased
    Co-authored-by: marcoschwepp

--- tests/CookieTest.php
+++ tests/CookieTest.php
@@ -103,109 +103,110 @@ final class CookieTest extends TestCase
 
         $cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
 
-        $result = @$cookie->save();
+        $result = $cookie->save();
 
         self::assertNull($result);
     }
 
-	/**
-	 * @dataProvider \Ergebnis\DataProvider\StringProvider::arbitrary()
-	 */
-	public function testDeleteWhenCookieWasNotSaved(string $name): void
-	{
-		$options = [
-			'name' => $name,
-		];
+    /**
+     * @dataProvider \Ergebnis\DataProvider\StringProvider::arbitrary()
+     */
+    public function testDeleteWhenCookieWasNotSaved(string $name): void
+    {
+        $options = [
+            'name' => $name,
+        ];
 
-		$cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
-		$cookie->delete();
+        $cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
+        $cookie->delete();
 
-		self::assertArrayNotHasKey($name, $_COOKIE);
-	}
+        self::assertArrayNotHasKey($name, $_COOKIE);
+    }
 
-	/**
-	 * @dataProvider \Ergebnis\DataProvider\StringProvider::arbitrary()
-	 */
-	public function testSetCookieValue(string $name): void
-	{
-		$faker = Faker\Factory::create();
+    /**
+     * @dataProvider \Ergebnis\DataProvider\StringProvider::arbitrary()
+     */
+    public function testSetCookieValue(string $name): void
+    {
+        $faker = Faker\Factory::create();
 
-		$options = [
-			'name' => $name,
-		];
-		$value = $faker->text(20);
+        $options = [
+            'name' => $name,
+        ];
+        $value = $faker->text(20);
 
-		$cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
-		$cookie->setValue($value);
+        $cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
+        $cookie->setValue($value);
 
-		self::assertSame($value, $cookie->getValue());
-	}
+        self::assertSame($value, $cookie->getValue());
+    }
 
-	/**
-	 * @dataProvider \Ergebnis\DataProvider\StringProvider::arbitrary()
-	 */
-	public function testSetAndGetExpiresAt(string $name): void
-	{
-		$options = [
-			'name' => $name,
-		];
+    /**
+     * @dataProvider \Ergebnis\DataProvider\StringProvider::arbitrary()
+     */
+    public function testSetAndGetExpiresAt(string $name): void
+    {
+        $options = [
+            'name' => $name,
+        ];
 
-		$faker = Faker\Factory::create();
+        $faker = Faker\Factory::create();
 
-		$expires = new DateTimeImmutable($faker->date('Y-m-d H:i:s', '+1 year'));
+        $expires = new DateTimeImmutable($faker->date('Y-m-d H:i:s', '+1 year'));
 
-		$cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
+        $cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
 
-		$cookie->expiresAt($expires);
+        $cookie->expiresAt($expires);
 
-		self::assertSame($cookie->getExpiresAt(), $expires);
-	}
+        self::assertSame($cookie->getExpiresAt(), $expires);
+    }
 
-	/**
-	 * @dataProvider \Ergebnis\DataProvider\StringProvider::arbitrary()
-	 */
-	public function testSetExpiresIn(string $name): void
-	{
-		$options = [
-			'name' => $name,
-		];
-		$faker = Faker\Factory::create();
-		$cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
-		$seconds = $faker->numberBetween(1000, 999999999);
-		$cookie->expiresIn($seconds);
-		$expectedDate = new \DateTimeImmutable(sprintf('+ %s seconds', $seconds));
+    /**
+     * @dataProvider \Ergebnis\DataProvider\StringProvider::arbitrary()
+     */
+    public function testSetExpiresIn(string $name): void
+    {
+        $options = [
+            'name' => $name,
+        ];
+        $faker = Faker\Factory::create();
+        $cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
+        $seconds = $faker->numberBetween(1000, 999999999);
+        $cookie->expiresIn($seconds);
+        $expectedDate = new \DateTimeImmutable(\sprintf('+ %s seconds', $seconds));
 
-		self::assertSame($expectedDate->format('Y-m-d H:i:s'), $cookie->getExpiresAt()->format('Y-m-d H:i:s'));
-	}
+        self::assertSame($expectedDate->format('Y-m-d H:i:s'), $cookie->getExpiresAt()->format('Y-m-d H:i:s'));
+    }
 
-	/**
-	 * @dataProvider expiredDataProvider
-	 */
-	public function testCookieIsExpired(string $modifier, bool $expected): void
-	{
-		$faker = \Faker\Factory::create();
-		$options = [
-			'name' => $faker->word(),
-		];
-		$cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
+    /**
+     * @dataProvider expiredDataProvider
+     */
+    public function testCookieIsExpired(string $modifier, bool $expected): void
+    {
+        $faker = \Faker\Factory::create();
+        $options = [
+            'name' => $faker->word(),
+        ];
+        $cookie = \marcoschwepp\Cookie\Cookie::constructFromOptions($options);
 
-		$cookie->expiresAt(new \DateTimeImmutable($modifier));
+        $cookie->expiresAt(new \DateTimeImmutable($modifier));
 
-		self::assertSame($expected, $cookie->isExpired());
-	}
+        self::assertSame($expected, $cookie->isExpired());
+    }
 
-	public function expiredDataProvider(): array
-	{
-		$faker = Faker\Factory::create();
-		$days = $faker->numberBetween(2, 999);
-		$minutes = $faker->numberBetween(1, 59);
-		return [
-			[\sprintf('- %s days', $days), true],
-			[\sprintf('+ %s days', $days), false],
-			['- 1 day', true],
-			['+ 1 day', false],
-			[\sprintf('- %s minutes', $minutes), true],
-			[\sprintf('+ %s minutes', $minutes), false],
-		];
-	}
+    public function expiredDataProvider(): array
+    {
+        $faker = \Faker\Factory::create();
+        $days = $faker->numberBetween(2, 999);
+        $minutes = $faker->numberBetween(1, 59);
+
+        return [
+            [\sprintf('- %s days', $days), true],
+            [\sprintf('+ %s days', $days), false],
+            ['- 1 day', true],
+            ['+ 1 day', false],
+            [\sprintf('- %s minutes', $minutes), true],
+            [\sprintf('+ %s minutes', $minutes), false],
+        ];
+    }
 }
